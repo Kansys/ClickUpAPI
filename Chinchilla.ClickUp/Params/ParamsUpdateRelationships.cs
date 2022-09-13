6bcb206ac32302bd2fc167963d6f9fecc4e59efb
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2022-09-13 13:44:58 +0300

    Add a DTO for updating of relationship tasks

---
+++ Chinchilla.ClickUp/Params/ParamsUpdateRelationships.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Chinchilla.ClickUp.Params
+{
+    public class ParamsUpdateRelationships
+    {
+        [JsonProperty("add")]
+        public string[] TasksToAdd { get; }
+
+        [JsonProperty("rem")]
+        public string[] TasksToRemove { get; }
+
+        public ParamsUpdateRelationships(string[] tasksToAdd, string[] tasksToRemove)
+        {
+            TasksToAdd = tasksToAdd;
+            TasksToRemove = tasksToRemove;
+        }
+        public ParamsUpdateRelationships(string[] tasksToAdd): this(tasksToAdd, Array.Empty<string>())
+        {
+        }
+
+        public override string ToString()
+        {
+            var toRemove = TasksToRemove.Any() ? $" To remove: {string.Join(',', TasksToRemove)}." : "";
+            return $"To add: {string.Join(',', TasksToAdd)}.{toRemove}";
+        }
+    }
+}
